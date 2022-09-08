cec83ace77e90197ca80786e39102739f27a7647
tfikadu <tfikadu76@gmail.com>
2022-09-08 11:46:24 -0700

    Task5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - Printing
  *
@@ -6,7 +6,6 @@
  */
 int main(void)
 {
-
-	puts("\ "Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
