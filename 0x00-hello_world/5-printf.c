36a0f05cf352d6b0c0789bd202e25aac742f104f
tfikadu <tfikadu76@gmail.com>
2022-09-08 12:15:09 -0700

    Task6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,21 +1,11 @@
-#include <stdio.h>                                                                                                              
-
-/**                                                                                                                             
+#include <stdio.h>
+/**
+ * main - Print a text
  *
- *  * main - Print a text                                                                                                              
- *
- *   *                                                                                                                              
- *
- *    * Return: Always 0 (Success)                                                                                                
- *
- *     */                                                                                                                             
-
-int main(void)                                                                                                                  
-
-{                                                                                                                               
-
-	        printf("with proper grammar,but the outcome is a piece of art,\n");                                                           
-
-		        return (0);                                                                                                             
-
-}    
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar,but the outcome is a pieceof art,\n");
+	return (0);
+}
