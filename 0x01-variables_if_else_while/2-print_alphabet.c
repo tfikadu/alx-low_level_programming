b0b41a5fbc617a5d1bb1e184920bd42869a0c0b9
tfikadu <tfikadu76@gmail.com>
2022-09-10 04:52:53 -0700

    Task7

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
 /**
- * main-lowercase alphabets
+ * main - lowercase alphabets
  *
- * Return:Always (Success)
+ * Return: Always (Success)
  */
 int main(void)
 {
 	char c;
 
-	for (c ='a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
 
 	putchar('\n');
 	return (0);
 }
+
 
