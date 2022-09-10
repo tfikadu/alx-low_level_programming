02eaaf583083e1f38cb208280e6fae5acc707706
tfikadu <tfikadu76@gmail.com>
2022-09-10 02:56:58 -0700

    Task1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,9 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
 
 /**
- * main - Prints if number is positive, zero or negative
+ * main - Prints if number is positive,zero or negative
  *
  * Return: Always (Success)
  */
