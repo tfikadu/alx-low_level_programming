656ef799dcd137ac9f1cb2e443ec01f5fbd577a6
tfikadu <tfikadu76@gmail.com>
2022-09-10 02:43:38 -0700

    Task1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,14 +5,14 @@
 /**
  * main - Prints if number is positive,zero or negative
  *
- * Return: Always (Success)
+ * Return:Always (Success)
  */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	n = rand() - RAND_MAX/2;
 
 	if (n > 0)
 	{
