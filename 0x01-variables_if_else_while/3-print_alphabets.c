abe103ba1e01546730e9352f928dadec63957b75
tfikadu <tfikadu76@gmail.com>
2022-09-10 05:59:44 -0700

    Task13

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	char c;
 
-	for (c = 'a'; c <= 'z'; c++)
+	for (c='a'; c<='z'; c++)
 	{
 		putchar(c);
 	}
-	for (c = 'A'; c <= 'z'; c++)
+	for (c='A'; c<='z'; c++)
 	{
 		putchar(c)
 	}
