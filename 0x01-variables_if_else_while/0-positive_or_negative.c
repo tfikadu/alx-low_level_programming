121e7c77e67ebbd3b3f39ba601f5e44910e62f89
tfikadu <tfikadu76@gmail.com>
2022-09-10 02:40:36 -0700

    Task1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,27 +5,28 @@
 /**
  * main - Prints if number is positive,zero or negative
  *
- * Return:Always (Success)
- * /
- int main(void)
- {
- int n;
-
- srand(time(0));
- n = rand() - RAND_MAX/2;
-
- if (n > 0)
- {
-printf("%d is positive\n",n);
-}
-else if (n == 0)
+ * Return: Always (Success)
+ */
+int main(void)
 {
-printf("%d is zero\n",n);
-}
-else
-{
-printf("%d is negative\n",n);
-}
-return (0);
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n",n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n",n);
+	}
+	else
+	{
+		printf("%d is negative\n",n);
+	}
+
+	return (0);
 }
 
