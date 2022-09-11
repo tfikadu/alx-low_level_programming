e4e36fab89981a36ed5248fc65ad5acc486fe4d8
tfikadu <tfikadu76@gmail.com>
2022-09-11 14:29:17 -0700

    Task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print the numbers since 0 to 9
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9',<F2> n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
