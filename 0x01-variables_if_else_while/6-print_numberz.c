be177ee3827d6521a7996d6e62a3142187b2ba88
tfikadu <tfikadu76@gmail.com>
2022-09-11 14:33:02 -0700

    Task6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Print the numbers since 0 to 9
+ * main - Prints the numbers since 0 to 9
  *
  * Return: Always (Success)
  */
@@ -9,7 +9,7 @@ int main(void)
 {
 	int n;
 
-	for (n = '0'; n <= '9',<F2> n++)
+	for (n = '0'; n <= '9'; n++)
 	{
 		putchar(n);
 	}
