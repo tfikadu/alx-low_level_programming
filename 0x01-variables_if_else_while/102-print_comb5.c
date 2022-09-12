66ee6a46a01e8ef7625d510ee253230b866c4e48
tfikadu <tfikadu76@gmail.com>
2022-09-12 08:04:59 -0700

    Task12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,13 +16,13 @@ int main(void)
 			{
 				putchar((i / 10) + '0');
 				putchar((i % 10) + '0');
-				putchar('');
+				putchar(' ');
 				putchar((p / 10) + '0');
 				putchar((p % 10) +'0');
 				if (i != 98)
 				{
 					putchar(',');
-					putchar('');
+					putchar(' ');
 				}
 			}
 		}
