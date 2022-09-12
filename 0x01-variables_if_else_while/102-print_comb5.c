4a5ff8463c09deec26460caa9492358c4d6e8bfc
tfikadu <tfikadu76@gmail.com>
2022-09-12 08:03:21 -0700

    Task12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
- * main - print possible combination of two 2-digit 
- * numbers
+ * main - print possible combination of two 2-digit numbers
  *
  * Return: returns zero at the end
  */
@@ -17,13 +16,13 @@ int main(void)
 			{
 				putchar((i / 10) + '0');
 				putchar((i % 10) + '0');
-				putchar(' ');
+				putchar('');
 				putchar((p / 10) + '0');
 				putchar((p % 10) +'0');
 				if (i != 98)
 				{
 					putchar(',');
-					putchar(' ');
+					putchar('');
 				}
 			}
 		}
