5382b39145753a7acbf76b2120f9321ef5a7de48
tfikadu <tfikadu76@gmail.com>
2022-09-12 04:07:29 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,11 +13,11 @@ int main(void)
 	for (d = 0; d < 100; d++)
 	{
 		putchar((d / 10) + '0');
-		putchar((d % 10) + '0);
+		putchar((d % 10) + '0');
 		if (d != 99)
 		{
-			putchar(' , ');
-			putchar('');
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
