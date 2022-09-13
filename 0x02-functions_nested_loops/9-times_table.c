7093ac4fed71690cf86f097b49ef1ccb103c35fe
tfikadu <tfikadu76@gmail.com>
2022-09-13 11:48:24 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,14 +14,12 @@ void times_table(void)
 		{
 			_putchar(',');
 			_putchar(' ');
-
 			prod = num * mult;
-
 			if (prod <= 9)
+
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
-		
 			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
