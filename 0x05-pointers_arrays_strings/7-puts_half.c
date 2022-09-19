7300f33da2d508d1865be89c734a4401e092c66b
tfikadu <tfikadu76@gmail.com>
2022-09-19 12:14:22 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -4,23 +4,19 @@
 /**
  * puts_half - prints the second half of a string
  * @str: string to print
- *
- * Return: void
  */
 void puts_half(char *str)
 {
-	int j, i = 0;
+	int x, y, i;
 
-	while (*(str + i))
-		i++;
-	j = i / 2;
-	if (i % 2)
-		j += 1;
-	while (j < i)
-	{
-		_putchar(*(str + j))
-			j++;
-	}
-	_putchar('\n');
+	x = strlen(str);
+	if (x % 2 == 1)
+		y = x / 2 + 1;
+	else
+		y = x / 2
+			for (i = y; i < x; i++)
+				putchar(str[i]);
+	putchar('\n');
 }
+
 
