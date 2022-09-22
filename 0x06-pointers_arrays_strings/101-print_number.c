9964c1dbf4cb2cc85265478f8cbb7e6a46be4bcf
tfikadu <tfikadu76@gmail.com>
2022-09-22 13:16:34 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,34 +2,26 @@
 
 /**
  * print_number - function prints an integer
- *
  * @n: integer
  *
  * Return: nothing
  */
-
 void print_number(int n)
 {
-	int y = 1000000000;
-	int x, z;
+	unsigned int x;
 
-	for (x = 0; x < 10; x++)
+	if (n < 0)
 	{
-		if ((n / y) == 0 && z == 0)
-		{
-			y = (y / 10);
-			continue;
-		}
-		else if (z == 0)
-		{
-			_putchar((n / y) + '0');
-			z++;
-		}
-		else
-		{
-			_putchar ((n / y) % 10 + '0');
-		}
-		y = (y / 10);
+		_putchar('-');
+		n *= -1;
 	}
+
+	x = n
+
+		if (x / 10)
+			print_number(x / 10);
+
+	 _putchar(x % 10 + '0');
 }
+
 
