b791dc810454d36bc15ff1d35558d930cf9e9c78
tfikadu <tfikadu76@gmail.com>
2022-10-12 10:58:15 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,13 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include "function_pointers.h"
 /**
- * print_name - f points to either func in main
- * @name: name
- * @f: pointer to print upp or non upp func
- * Return: void
+ * print_name - prints a name.
+ * @name: pointer to name.  
+ * @f: function pointer.  
+ *
+ * Return: void.
  */
-void print_name(char *name, void (*f)(char *))
+void print_name(char *name, void (*f)(char *)) 
 {
-	if (name == NULL || f == NULL)
-		return;
-	return;
+	if (name && f) 
+		f(name); 
 }
 
