614bdb352cb35f0b33d2bcceef9a3c40f9f9fc35
tfikadu <tfikadu76@gmail.com>
2022-10-27 14:13:40 -0700

    100-get_endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,22 +1,18 @@
-#define LITTLE_ENDIAN 1
-#define BIG_ENDIAN 0
-
-/* BIG_ENDIAN and LITTLE_ENDIAN are also defined seperately in stdlib.h */
+#include "main.h"
 
 /**
- * get_endianness - determines whether complie environment
- * is big-endian (MSB pointed to) or little-endian (LSB pointed to)
+ * get_endianness - checks the endianness
  *
- * Return: 0 if big-endian, 1 if little-endian
+ * Return: 0 if big endian, 1 if little endian
  */
-
 int get_endianness(void)
 {
-	int test_num;
-	char *c_ptr;
+	unsigned int x;
+	char *c;
 
-	test_num = 1;
-	c_ptr = (char *)&test_num;
-	return (c_ptr[0] ? LITTLE_ENDIAN : BIG_ENDIAN);
+	x = 1;
+	c = (char *) &x;
+
+	return ((int)*c);
 }
 
